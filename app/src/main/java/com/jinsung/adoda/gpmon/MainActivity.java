3cd00cd88ae7d726bd536ab2b4b3f3c9cda629bb
adodaorbit <adodaorbit@gmail.com>
2015-12-12 17:17:44 +0900

    Initial commit

---
+++ app/src/main/java/com/jinsung/adoda/gpmon/MainActivity.java
@@ -0,0 +1,55 @@
+package com.jinsung.adoda.gpmon;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+
+import cz.msebera.android.httpclient.Header;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AsyncHttpClient client = new AsyncHttpClient();
+                client.get("http://portalauthmon.azurewebsites.net/test.php", new JsonHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                    }
+
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                        TextView view = (TextView)findViewById(R.id.textview);
+                        try {
+                            for (int i = 0; i < response.length(); i++) {
+                                view.append(new Test(response.getJSONObject(i)).toString());
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/jinsung/adoda/gpmon/Test.java
@@ -0,0 +1,38 @@
+package com.jinsung.adoda.gpmon;
+
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import static android.widget.Toast.*;
+
+/**
+ * Created by adoda on 2015-12-12.
+ */
+public class Test {
+    private String name;
+    private int age;
+
+    public Test(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Test(JSONObject obj) throws JSONException {
+        this.name = obj.getString("name");
+        this.age = obj.getInt("age");
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public String toString() {
+        return "name: " + this.name + ", age: " + this.age + "\n";
+    }
+}
